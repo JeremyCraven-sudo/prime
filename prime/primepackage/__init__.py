35764e16bcf33c9eb3774c35a082451b52fd9a60
JeremyCraven-sudo <60520433+JeremyCraven-sudo@users.noreply.github.com>
2020-03-09 20:55:30 -0400

    Add files via upload

---
+++ prime/primepackage/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+'''Imports the functions from the package
+ 
+Imports get_n_prime, write_primes, and read_primes from the primepackage
+ 
+'''
+ 
+__author__ = "Jeremy Craven"
+__license__ = "GPL"
+__version__ = "0.1"
+__email__ = "jecraven@valdosta.edu"
+__status__ = "Finished"
+
+from .primemodule import get_n_prime
+from .primeio import write_primes
+from .primeio import read_primes
